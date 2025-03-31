cb6d0c7f9cc8747385ddd23ae12ee686e2fc4732
Karthi-nachimuthu <92216175+Karthi-nachimuthu@users.noreply.github.com>
2025-03-31 23:48:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
 
-    let validUsername = "admin";
-    let validPassword = "12345";
+    let validUsername = "Karthi";
+    let validPassword = "7615";
 
     if (username === validUsername && password === validPassword) {
         document.getElementById("loginContainer").style.display = "none";
